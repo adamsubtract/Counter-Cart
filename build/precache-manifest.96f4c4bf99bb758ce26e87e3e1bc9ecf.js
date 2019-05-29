7454ed683d9f51f3eaac242a5ad6a4ff372d8160
Adam <adam@Adams-MacBook-Pro.local>
2019-05-28 22:25:24 -0700

    added minus button

---
+++ build/precache-manifest.96f4c4bf99bb758ce26e87e3e1bc9ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b47b192012a3150081bfa6de4c0759",
+    "url": "/Counter-Cart/index.html"
+  },
+  {
+    "revision": "4782148edff60aa1294b",
+    "url": "/Counter-Cart/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "495a7101db2e05543c8b",
+    "url": "/Counter-Cart/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "4782148edff60aa1294b",
+    "url": "/Counter-Cart/static/js/2.5b44faae.chunk.js"
+  },
+  {
+    "revision": "495a7101db2e05543c8b",
+    "url": "/Counter-Cart/static/js/main.aa3344d6.chunk.js"
+  },
+  {
+    "revision": "6609a7ea42b3de6b77a7",
+    "url": "/Counter-Cart/static/js/runtime~main.6dbf9519.js"
+  }
+]);
